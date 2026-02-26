be61af19d4710daeb5b7ff2b8502519f569efe25
fabenan-f <63860771+fabenan-f@users.noreply.github.com>
2026-02-26 10:07:11 +0100

    fix: restart project from scratch
    To enable more flexible development, most of the existing modules were removed. A backup copy exists from which we can incrementally cherry-pick common patterns that need to be present in the service.
    
    PR: #72

--- kmip/payloads/doc.go
+++
@@ -1,4 +0,0 @@
-// Package payloads provides types and functions for handling KMIP protocol payloads.
-// It defines the structures and logic required to encode, decode, and manipulate
-// KMIP request and response payloads as specified by the Key Management Interoperability Protocol.
-package payloads
